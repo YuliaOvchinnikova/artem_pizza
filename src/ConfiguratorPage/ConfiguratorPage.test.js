e4d205d7bd497091335c7119861e5775f38b7b81
Yulia Ovchinnikova <yulia.ovchinnikova.92@gmail.com>
2020-11-22 16:09:28 +0200

    Add tests

--- src/ConfiguratorPage/ConfiguratorPage.test.js
+++ src/ConfiguratorPage/ConfiguratorPage.test.js
@@ -1,16 +1,26 @@
-// import { fireEvent, render } from "@testing-library/react";
 const { fireEvent, render } = require("@testing-library/react");
-const { Configurator } = require("./ConfiguratorPage");
+const { ConfiguratorPage } = require("./ConfiguratorPage");
+import { PizzaProvider, PizzaContext } from "./../PizzaContext";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
 
-describe("Configurator", () => {
+describe("ConfiguratorPage", () => {
   it("renders correctly", () => {
-    const { getByText } = render(<Configurator />);
+    const { getByText } = render(
+      <PizzaProvider>
+        <ConfiguratorPage />
+      </PizzaProvider>
+    );
     expect(getByText("Choose pizza size:")).toBeInTheDocument();
   });
 
   describe("with large size selected", () => {
     it("shows correct sum", () => {
-      const { getByText } = render(<Configurator />);
+      const { getByText } = render(
+        <PizzaProvider>
+          <ConfiguratorPage />
+        </PizzaProvider>
+      );
 
       fireEvent.click(getByText("35 sm"));
 
@@ -20,7 +30,11 @@ describe("Configurator", () => {
 
   describe("with thick dough selected", () => {
     it("shows correct sum", () => {
-      const { getByText } = render(<Configurator />);
+      const { getByText } = render(
+        <PizzaProvider>
+          <ConfiguratorPage />
+        </PizzaProvider>
+      );
 
       fireEvent.click(getByText("thick"));
 
@@ -30,7 +44,11 @@ describe("Configurator", () => {
 
   describe("with hot sauce selected", () => {
     it("shows correct sum", () => {
-      const { getByText } = render(<Configurator />);
+      const { getByText } = render(
+        <PizzaProvider>
+          <ConfiguratorPage />
+        </PizzaProvider>
+      );
 
       fireEvent.click(getByText("hot sauce"));
 
@@ -40,7 +58,11 @@ describe("Configurator", () => {
 
   describe("with all toppings checked", () => {
     it("shows maximum sum", () => {
-      const { getByText } = render(<Configurator />);
+      const { getByText } = render(
+        <PizzaProvider>
+          <ConfiguratorPage />
+        </PizzaProvider>
+      );
 
       fireEvent.click(getByText("mozzarella"));
       fireEvent.click(getByText("cheddar"));
@@ -60,9 +82,14 @@ describe("Configurator", () => {
 
   describe("on pizza submit", () => {
     it("passes constructed pizza", () => {
-      const onPizzaSubmit = jest.fn();
+      const history = createMemoryHistory();
+      const setPizza = jest.fn();
       const { getByText } = render(
-        <Configurator onPizzaSubmit={onPizzaSubmit} />
+        <Router history={history}>
+          <PizzaContext.Provider value={{ setPizza }}>
+            <ConfiguratorPage />
+          </PizzaContext.Provider>
+        </Router>
       );
 
       fireEvent.click(getByText("35 sm"));
@@ -80,7 +107,7 @@ describe("Configurator", () => {
 
       fireEvent.click(getByText("Your order: 424 RUB"));
 
-      expect(onPizzaSubmit).toBeCalledWith({
+      expect(setPizza).toBeCalledWith({
         dough: "thin",
         ingredients: [
           "cheddar",
@@ -93,6 +120,7 @@ describe("Configurator", () => {
         sauce: "hot",
         size: "large",
       });
+      expect(history.location.pathname).toEqual("/order");
     });
   });
 });
--- src/OrderPage/OrderPage.test.js
+++ src/OrderPage/OrderPage.test.js
@@ -1,31 +1,33 @@
 const { render } = require("@testing-library/react");
-const { Order } = require("./OrderPage");
+import { OrderPage } from "./OrderPage";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
+import { PizzaContext } from "./../PizzaContext";
 
-describe("Order", () => {
+describe("OrderPage", () => {
   it("renders correctly", () => {
     const history = createMemoryHistory();
+    const pizza = {
+      size: "small",
+      dough: "thin",
+      sauce: "tomato",
+      ingredients: [
+        "mozzarella",
+        "cheddar",
+        "dorblu",
+        "tomato",
+        "mushrooms",
+        "paprika",
+        "bacon",
+        "pepperoni",
+        "ham",
+      ],
+    };
     const { getByText } = render(
       <Router history={history}>
-        <Order
-          pizza={{
-            size: "small",
-            dough: "thin",
-            sauce: "tomato",
-            ingredients: [
-              "mozzarella",
-              "cheddar",
-              "dorblu",
-              "tomato",
-              "mushrooms",
-              "paprika",
-              "bacon",
-              "pepperoni",
-              "ham",
-            ],
-          }}
-        />
+        <PizzaContext.Provider value={{ pizza }}>
+          <OrderPage />
+        </PizzaContext.Provider>
       </Router>
     );
 

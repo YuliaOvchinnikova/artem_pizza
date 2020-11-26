fe626569f18b6b7f244c4a6b25b4ed16eb4c4e55
Yulia Ovchinnikova <yulia.ovchinnikova.92@gmail.com>
2020-11-26 18:56:58 +0200

    Add forms to RegistrationPage and LoginPage, PaymentPage

--- src/PaymentPage/PaymentPage.js
+++ src/PaymentPage/PaymentPage.js
@@ -1,9 +1,54 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+const normalizeCardNumber = (value) => {
+  return (
+    value
+      .replace(/\s/g, "")
+      .match(/.{1,4}/g)
+      ?.join(" ")
+      .substr(0, 19) || ""
+  );
+};
 
 export const PaymentPage = () => {
+  const { register, handleSubmit } = useForm();
+  const [cardType, setCardType] = useState("");
+
+  const onSubmit = (data) => {
+    if (data.cardNumber[0] === "4") {
+      setCardType("Visa");
+    } else if (data.cardNumber[0] === "5") {
+      setCardType("MasterCard");
+    } else {
+      setCardType("");
+    }
+  };
+
   return (
     <>
       <h1>Payment page</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="cardNumber">Card Number:</label>
+          <input
+            placeholder="0000 0000 0000 0000"
+            type="tel"
+            inputMode="numeric"
+            autoComplete="cc-number"
+            name="cardNumber"
+            id="cardNumber"
+            onChange={(event) => {
+              const { value } = event.target;
+              event.target.value = normalizeCardNumber(value);
+            }}
+            ref={register}
+          />
+          <p>{cardType}</p>
+        </div>
+        <button>Submit</button>
+      </form>
       <Link to="/bill">Bill</Link>
     </>
   );
